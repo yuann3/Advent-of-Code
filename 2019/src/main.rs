beb24d6548c9dfdddfac7b318304003f792e719b
Yiyuan Li <yy@eyuan.me>
2024-11-13 09:42:55 +0800

    add day1 solution for 2019

---
+++ 2019/src/main.rs
@@ -0,0 +1,39 @@
+use std::env;
+use std::error::Error;
+
+mod day1;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 1 {
+        let day = &args[1][3..];
+        run_day(day)?;
+    } else {
+        run_all_days()?;
+    }
+
+    Ok(())
+}
+
+fn run_day(day: &str) -> Result<(), Box<dyn Error>> {
+    match day {
+        "1" => {
+            println!("Day 1:");
+            println!("Part 1: {}", day1::part1::solve()?);
+            println!("Part 2: {}", day1::part2::solve()?);
+        }
+        _ => println!("Day not implemented"),
+    }
+    Ok(())
+}
+
+fn run_all_days() -> Result<(), Box<dyn Error>> {
+    for day in 1..=25 {
+        if let Err(e) = run_day(&day.to_string()) {
+            eprintln!("Error running day {}: {}", day, e);
+        }
+        println!();
+    }
+    Ok(())
+}
