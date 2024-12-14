0ec3ac9ef4d74d2f997277c0e20e6c4bd0e99a84
Yiyuan Li <yy@eyuan.me>
2024-12-14 16:15:30 +0800

    ok

--- 2019/src/main.rs
+++ 2019/src/main.rs
@@ -23,11 +23,10 @@ fn run_day(day: &str) -> Result<(), Box<dyn Error>> {
             println!("Day 1:");
             println!("Part 1: {}", day1::part1::solve()?);
             println!("Part 2: {}", day1::part2::solve()?);
-        },
+        }
         "2" => {
             println!("Day 2:");
             println!("Part 1: {}", day2::part1::solve()?);
-            //println!("Part 2: {}", day2::part2::solve()?);
         }
         _ => println!("Day not implemented"),
     }
