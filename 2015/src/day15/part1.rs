a5f51a92d927ed1596ecee734c375ef6e8c0ddbf
Yiyuan Li <yy@eyuan.me>
2025-06-08 14:23:46 +0800

    finished part 1

--- 2015/src/day15/part1.rs
+++ 2015/src/day15/part1.rs
@@ -3,7 +3,6 @@ use aoc_lib::read_lines;
 
 #[derive(Debug)]
 struct Ingredient {
-    name: String,
     capacity: i32,
     durability: i32,
     flavor: i32,
@@ -12,15 +11,75 @@ struct Ingredient {
 
 impl Ingredient {
     fn parse_line(line: &str) -> Result<Self> {
-        todo!()
+        let parts: Vec<&str> = line.split(&[' ', ',', ':']).collect();
+
+        let mut capacity = 0;
+        let mut durability = 0;
+        let mut flavor = 0;
+        let mut texture = 0;
+
+        for i in 0..parts.len() {
+            match parts[i] {
+                "capacity" => capacity = parts[i + 1].parse()?,
+                "durability" => durability = parts[i + 1].parse()?,
+                "flavor" => flavor = parts[i + 1].parse()?,
+                "texture" => texture = parts[i + 1].parse()?,
+                _ => continue,
+            }
+        }
+
+        Ok(Ingredient {
+            capacity,
+            durability,
+            flavor,
+            texture,
+        })
     }
+}
+
+fn calculate_score(ingredients: &[Ingredient], amounts: &[i32; 4]) -> i32 {
+    let mut capacity = 0;
+    let mut durability = 0;
+    let mut flavor = 0;
+    let mut texture = 0;
+
+    for (ingredient, &amount) in ingredients.iter().zip(amounts.iter()) {
+        capacity += ingredient.capacity * amount;
+        durability += ingredient.durability * amount;
+        flavor += ingredient.flavor * amount;
+        texture += ingredient.texture * amount;
+    }
+
+    capacity = capacity.max(0);
+    durability = durability.max(0);
+    flavor = flavor.max(0);
+    texture = texture.max(0);
+
+    capacity * durability * flavor * texture
 }
 
 pub fn solve() -> Result<i32> {
     let lines = read_lines("input/day15.in")?;
-    for line in lines {
-        println!("{}", line);
+
+    let ingredients: Result<Vec<Ingredient>, _> = lines
+        .iter()
+        .map(|line| Ingredient::parse_line(line))
+        .collect();
+
+    let ingredients = ingredients?;
+
+    let mut max_score = 0;
+    for a in 0..=100 {
+        for b in 0..=(100 - a) {
+            for c in 0..=(100 - a - b) {
+                let d = 100 -a -b -c;
+
+                let amounts = [a, b, c, d];
+                let score = calculate_score(&ingredients, &amounts);
+                max_score = max_score.max(score)
+            }
+        }
     }
 
-    Ok(09200622)
+    Ok(max_score)
 }
